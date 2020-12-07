ce59b4de87bcc71d6b9bae5ab5235ae3f48d4c7b
yunfei.wang <yunfei.wang@vhall.com>
2020-12-07 16:24:18 +0800

    更改默认房间

--- const/const.js
+++ const/const.js
@@ -1,6 +1,6 @@
 export default {
-  channelId: 'ch_1a348b67', //需要将此处inavId改为自己的
+  channelId: 'ch_b9dd840e', //需要将此处inavId改为自己的
   appId: 'd317f559', //需要将此处appId改为自己的
   accountId: `miniProgram_${Math.floor(1000 + Math.random() * 9000)}`, //需要将此处accountId改为自己的
-  token: 'access:d317f559:75107dced08acdb1' //需要将此处token改为自己的
+  token: 'access:d317f559:b3acfa862ae09232' //需要将此处token改为自己的
 }
